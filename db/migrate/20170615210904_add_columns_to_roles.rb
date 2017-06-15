ed9452d6971d912330fc3594d1145e40fdb888ed
Shannan Muench <shannan.muench@gmail.com>
2017-06-15 18:10:38 -0400

    fixed migration file 20170615210904

--- db/migrate/20170615210904_add_columns_to_roles.rb
+++ db/migrate/20170615210904_add_columns_to_roles.rb
@@ -1,6 +1,6 @@
 class AddColumnsToRoles < ActiveRecord::Migration[5.1]
   def change
-    add_column :user, :user_id, :integer
-    add_column :quest, :quest_id, :integer
+    add_column :roles, :user_id, :integer
+    add_column :roles, :quest_id, :integer
   end
 end
