1f88fc7d0d84ab087c2ee114eabaab5041e018a4
Daniel Bilsker <daniel.bilsker@gmail.com>
2017-06-16 14:51:18 -0400

    add password_digest

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170615214330) do
+ActiveRecord::Schema.define(version: 20170616185020) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -58,6 +58,7 @@ ActiveRecord::Schema.define(version: 20170615214330) do
     t.string "phone"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
   end
 
   add_foreign_key "roles", "quests"
